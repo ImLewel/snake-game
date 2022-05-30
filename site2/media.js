a6368e408857b02821de53e883ac2d787cdeb329
Lev Pavelko <neroheroes6@gmail.com>
2022-05-30 07:31:58 +0300

    Some refactoring of 'media.js'

--- site2/media.js
+++ site2/media.js
@@ -1,15 +1,22 @@
 const myAudio = new Audio('./music/ambientMusic.mp3');
 const musicSlider = document.querySelector('#musicSlider');
 const musicPic = document.querySelector('#musicPic');
-myAudio.volume = 0.5;
+const divider = 10;
+const onOffPaths = {
+  off: './volumeIcons/volumeOff.png',
+  on: './volumeIcons/volumeOn.png',
+}
+myAudio.volume = 0;
+
 const setVolume = () => {
   musicSlider.oninput = () => {
-    myAudio.volume = musicSlider.value/10;
+    myAudio.volume = musicSlider.value/divider;
     if (myAudio.volume === 0) {
-      musicPic.src = './volumeIcons/volumeOff.png';
+      musicPic.src = onOffPaths.off;
       myAudio.muted = true;
+      myAudio.pause();
     } else {
-      musicPic.src = './volumeIcons/volumeOn.png';
+      musicPic.src = onOffPaths.on;
       myAudio.muted = false;
       myAudio.play();
     }
